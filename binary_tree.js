530b752a06d35f66cc04f5d3e75482bdede812b2
Zima220410 <47075629+Zima220410@users.noreply.github.com>
2022-02-22 17:00:33 +0200

    Add files via upload

--- binary_tree.js
+++ binary_tree.js
@@ -64,37 +64,42 @@ class Node {
         } else if (data > this.data) {
             this.right = this.right?.delete(data);
             return this;
-        } else {
-            this.left = this.left?.delete(data);
-            return this;
         }
+        this.left = this.left?.delete(data);
+        return this;
     }
 }
 
-function bubbleSort(array, callback) {
-    for (let i = 0; i < array.length - 1; i++) {
-        for (let j = 0; j < array.length - 1 - i; j++) {
-            if (callback(array[j], array[j + 1])){
-                let temp = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = temp;
+Array.prototype.bubbleSort = function (callback){
+    if (!Array.isArray(this)) {
+        throw new Error('Error, argument is not arrey');
+    }
+    for (let i = 0; i < this.length - 1; i++) {
+        for (let j = 0; j < this.length - 1 - i; j++) {
+            if (callback(this[j], this[j + 1])){
+                let temp = this[j + 1];
+                this[j + 1] = this[j];
+                this[j] = temp;
             }
         }
     }
-    return array;
+    return this;
 }
 
-function selectionSort(arrey, callback){
-    for (let i = 0; i < arrey.length - 1; i++){
+Array.prototype.selectionSort = function(callback){
+    if (!Array.isArray(this)) {
+        throw new Error('Error, argument is not arrey');
+    }
+    for (let i = 0; i < this.length - 1; i++){
         let min = i;
-        for (let j = i + 1; j < arrey.length; j++){
-            if (callback(arrey[j], arrey[min])){
+        for (let j = i + 1; j < this.length; j++){
+            if (callback(this[j], this[min])){
                 min = j;
             }
         }
-        let temp = arrey[min];
-        arrey[min] = arrey[i];
-        arrey[i] = temp;
+        let temp = this[min];
+        this[min] = this[i];
+        this[i] = temp;
     }
-    return arrey;
+    return this;
 }
